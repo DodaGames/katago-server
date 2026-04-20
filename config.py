283f8ca93185734ec833154f3a83ec886cf1ad00
박지수 <jisupark2000@gmail.com>
2026-04-20 22:42:01 +0900

    perf: adjust KataGo analysis thread configuration for improved throughput in macbook_air.cfg

--- config.py
+++ config.py
@@ -22,11 +22,11 @@ analysis_models = {
     "level4": "kata1-b6c96-s4136960-d1510003.txt.gz",  # rating 1524, 대기 (3이랑 비슷) (용호2)
     "level5": "kata1-b6c96-s5214720-d1690538.txt.gz",  # rating 1599,  (재호)
     "level6": "kata1-b6c96-s6127360-d1754797.txt.gz",  # rating 1697,  가닥 없음 (건호)
-    # "level7": "kata1-b6c96-s8080640-d1961030.txt.gz", # rating 1851,  가닥 약간 (무야호)
-    # "level8": "kata1-b6c96-s8982784-d2082583.txt.gz", # rating 1929,  통과 (중수 표본) (재호)
+    "level7": "kata1-b6c96-s8080640-d1961030.txt.gz",  # rating 1851,  가닥 약간 (무야호)
+    "level8": "kata1-b6c96-s8982784-d2082583.txt.gz",  # rating 1929,  통과 (중수 표본) (재호)
     # # "level9": "kata1-b6c96-s10014464-d2201128.txt.gz", # rating 2108,
     # # "level10": "kata1-b6c96-s10825472-d2300510.txt.gz", # rating 2283,
-    # "level11": "kata1-b6c96-s12849664-d2510774.txt.gz", # rating 2540, 통과 (고수 표본) (건호)
+    "level11": "kata1-b6c96-s12849664-d2510774.txt.gz",  # rating 2540, 통과 (고수 표본) (건호)
     # # "level12": "kata1-b6c96-s19408128-d3280178.txt.gz", # rating 4146, 기본기는 잡혀있음
     "level13": "kata1-b6c96-s48921344-d7092247.txt.gz",  # rating 8087, 최고수 표본 (무야호)
 }
@@ -36,17 +36,18 @@ katago_executable_path = get_env_required("KATAGO_EXECUTABLE_PATH")
 
 # 서빙할 모델 목록
 SERVING_MODELS = {
-    # "level2": "kata1-b6c96-s938496-d1208807.txt.gz",
-    # "level3": "kata1-b6c96-s1995008-d1329786.txt.gz",
+    "level2": "kata1-b6c96-s938496-d1208807.txt.gz",
+    "level3": "kata1-b6c96-s1995008-d1329786.txt.gz",
+    # "level6": "kata1-b6c96-s6127360-d1754797.txt.gz",  # rating 1697,  가닥 없음 (건호)
     "best": {
         "is_human": False,
         "main_model": "kata1-b10c128-s1141046784-d204142634.txt.gz",
     },
-    "human": {
-        "is_human": True,
-        "main_model": "kata1-b10c128-s1141046784-d204142634.txt.gz",
-        "human_model": "b18c384nbt-humanv0.bin.gz",
-    },
+    # "human": {
+    #     "is_human": True,
+    #     "main_model": "kata1-b10c128-s1141046784-d204142634.txt.gz",
+    #     "human_model": "b18c384nbt-humanv0.bin.gz",
+    # },
 }
 
 base_model_path = get_env_required("BASE_MODEL_PATH")
